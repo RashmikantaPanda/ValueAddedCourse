f85092ea21327dd9de0d6430376ae0c69631cd8d
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2022-12-11 15:09:00 +0530

    version 1.4

---
+++ JavaScript/validator.js
@@ -0,0 +1,42 @@
+function validate(e){
+    const name=document.getElementById("name").value;
+    const mobile=document.getElementById("mobile").value;
+    const email=document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    const password2=document.getElementById("password2").value;
+
+    console.log(name,mobile,email,password,password2);
+
+    const name_error=document.getElementById("name_error");
+    const mobile_error=document.getElementById("mobile_error");
+    const email_error=document.getElementById("email_error");
+    const password_error=document.getElementById("password_error");
+    const password2_error=document.getElementById("password2-error");
+    
+    let error=false;
+
+    if(name===""){
+        name_error.innerHTML="Name is required";
+        error=true;
+    }
+    else{
+        name_error.innerHTML="";
+    }
+
+    if(mobile===""){
+        mobile_error.innerHTML="MobileNo is required";
+        error=true;
+    }
+    else if(isNaN(mobile)||mobile.length!=10){
+        mobile_error.innerHTML="Please enter 10 digit valid mobile number";
+        error=true;
+    }
+    else{
+        mobile_error.innerHTML="";
+    }
+
+    if(error){
+        e.preventDefault();
+    }
+
+}
